37f9ff608d9101b00a6bcdc984188e3691dd2d6d
3amer <mahmedabdallah@fci.helwan.edu.eg>
2019-06-30 15:35:21 +0200

    Revamped hibernate.cfg.xml maping and hibernateUtil

--- src/main/java/hibernate/hibernatemaven/HibernateUtil.java
+++ src/main/java/hibernate/hibernatemaven/HibernateUtil.java
@@ -8,6 +8,8 @@ package hibernate.hibernatemaven;
 import java.io.File;
 import java.util.Properties;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
@@ -18,7 +20,8 @@ import org.hibernate.service.ServiceRegistry;
  * @author 3amerjr
  */
 public class HibernateUtil {
-        private static SessionFactory sessionFactory;
+
+    private static SessionFactory sessionFactory;
 
     public static SessionFactory getSessionFactory() {
 
@@ -26,7 +29,7 @@ public class HibernateUtil {
 
             try {
 
-                Configuration configuration = new Configuration().configure(new File("hibernate.cfg.xml"));
+         //       Configuration configuration = new Configuration().configure(new File("src/main/resources/hibernate.cfg.xml"));
           //      Configuration configuration = new Configuration();
 
                 // Hibernate settings equivalent to hibernate.cfg.xml's properties
@@ -54,12 +57,11 @@ public class HibernateUtil {
   //              configuration.setProperties(settings);
 
 
-                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure(new File("src/main/java/hibernate.cfg.xml")).build();
 
-                    .applySettings(configuration.getProperties()).build();
-
-                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-
+//                sessionFactory = configuration.//addAnnotatedClass(Person.class).
+//                        buildSessionFactory(serviceRegistry);
+                sessionFactory = new MetadataSources(serviceRegistry).buildMetadata().buildSessionFactory();
             } catch (Exception e) {
 
                 e.printStackTrace();
@@ -71,4 +73,16 @@ public class HibernateUtil {
         return sessionFactory;
 
     }
+//        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+//                .configure(new File("src/main/java/hibernate.cfg.xml")) // configures settings from hibernate.cfg.xml
+//                .build();
+//        try {
+//            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+//        } catch (Exception e) {
+//            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+//            // so destroy it manually.
+//            StandardServiceRegistryBuilder.destroy(registry);
+//        }
+//        return sessionFactory;
+//    }
 }
--- src/main/java/hibernate/hibernatemaven/Main.java
+++ src/main/java/hibernate/hibernatemaven/Main.java
@@ -5,8 +5,15 @@
  */
 package hibernate.hibernatemaven;
 
+
+import model.Person;
+import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
+import org.hibernate.Session;
 
 /**
  *
@@ -16,21 +23,37 @@ public class Main {
 
     public static void main(String[] args) {
         final  Logger logger = Logger.getLogger(Main.class);
-        
+        Session session =null;
 
         try {
              logger.info(
                      
-       "\n/------------------------------------------------------------------------------------------------------------------------------------------------------/");
-             
+       "\n------------------------------------------------------------------------------------------------------------------------------------------------------");
+       
             logger.info("Inside Try");
-            System.out.println(HibernateUtil.getSessionFactory().openSession().toString());
-            System.out.println(HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction().isActive());
+           session =  HibernateUtil.getSessionFactory().openSession();
+            System.out.println(session.toString());
+            session.getTransaction();
+            CriteriaBuilder cb =  session.getCriteriaBuilder();
+            CriteriaQuery<Person> cq =   cb.createQuery(Person.class);
+            Root<Person> root =  cq.from(Person.class);
+            cq.select(root);
+            List<Person> results =session.createQuery(cq).getResultList();
+                   
+            for (Person x : results)
+                System.out.println(x.getName());
         } catch (HibernateException e) {
           logger.error( "Errored Occured "+ e.toString());
         } finally {
-            HibernateUtil.getSessionFactory().getCurrentSession().close();
+           session.close();
+            logger.info("Current Session Closed");
             HibernateUtil.getSessionFactory().close();
+            System.out.println(HibernateUtil.getSessionFactory().isClosed());
+            logger.info("Session Factory Closed");
+            logger.info(
+                     
+       "\n------------------------------------------------------------------------------------------------------------------------------------------------------");
+             
         }
     }
 }
