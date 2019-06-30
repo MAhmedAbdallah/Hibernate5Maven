81ba8cacff445178fdfbb407651bb267341f3078
3amer <mahmedabdallah@fci.helwan.edu.eg>
2019-06-30 16:25:07 +0200

    DESC ordering with hibernate 5 instead of deprecated methods

--- src/main/java/hibernate/hibernatemaven/Main.java
+++ src/main/java/hibernate/hibernatemaven/Main.java
@@ -5,11 +5,11 @@
  */
 package hibernate.hibernatemaven;
 
-
 import model.Person;
 import java.util.List;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Order;
 import javax.persistence.criteria.Root;
 import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
@@ -22,38 +22,43 @@ import org.hibernate.Session;
 public class Main {
 
     public static void main(String[] args) {
-        final  Logger logger = Logger.getLogger(Main.class);
-        Session session =null;
+        final Logger logger = Logger.getLogger(Main.class);
+        Session session = null;
 
         try {
-             logger.info(
-                     
-       "\n------------------------------------------------------------------------------------------------------------------------------------------------------");
-       
+            logger.info(
+                    "\n------------------------------------------------------------------------------------------------------------------------------------------------------");
+
             logger.info("Inside Try");
-           session =  HibernateUtil.getSessionFactory().openSession();
+
+            session = HibernateUtil.getSessionFactory().openSession();
+
             System.out.println(session.toString());
+
             session.getTransaction();
-            CriteriaBuilder cb =  session.getCriteriaBuilder();
-            CriteriaQuery<Person> cq =   cb.createQuery(Person.class);
-            Root<Person> root =  cq.from(Person.class);
-            cq.select(root);
-            List<Person> results =session.createQuery(cq).getResultList();
-                   
-            for (Person x : results)
+
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+
+            CriteriaQuery<Person> cq = cb.createQuery(Person.class);
+
+            Root<Person> root = cq.from(Person.class);
+            cq.orderBy(cb.desc(root.get("id")));
+            List<Person> results = session.createQuery(cq).getResultList();
+
+            for (Person x : results) {
                 System.out.println(x.getName());
+            }
         } catch (HibernateException e) {
-          logger.error( "Errored Occured "+ e.toString());
+            logger.error("Errored Occured " + e.toString());
         } finally {
-           session.close();
+            session.close();
             logger.info("Current Session Closed");
             HibernateUtil.getSessionFactory().close();
             System.out.println(HibernateUtil.getSessionFactory().isClosed());
             logger.info("Session Factory Closed");
             logger.info(
-                     
-       "\n------------------------------------------------------------------------------------------------------------------------------------------------------");
-             
+                    "\n------------------------------------------------------------------------------------------------------------------------------------------------------");
+
         }
     }
 }
